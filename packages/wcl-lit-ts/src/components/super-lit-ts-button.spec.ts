1823550f15db18513324fde5e173445dfc2133c4
Al-un <alun.sng@gmail.com>
2021-07-08 13:29:15 +0900

    lit-ts: update button test

--- packages/wcl-lit-ts/src/components/super-lit-ts-button.spec.ts
+++ packages/wcl-lit-ts/src/components/super-lit-ts-button.spec.ts
@@ -1,22 +1,39 @@
-import { expect, fixture } from '@open-wc/testing';
+import { expect, fixture, oneEvent, waitUntil } from '@open-wc/testing';
 import './super-lit-ts-button';
+import { SuperLitTsButton } from './super-lit-ts-button';
 
-describe('super-lit-ts-button', () => {
+type ElementClass = SuperLitTsButton;
+const elementTag = `super-lit-ts-button`;
+
+describe(`${elementTag}`, () => {
   it('renders correctly', async () => {
-    const el = await fixture('<super-lit-ts-button>Plop</super-lit-ts-button>');
+    const el = await fixture<ElementClass>(
+      `<${elementTag}>Plop</${elementTag}>`
+    );
     expect(el.textContent).to.equal('Plop');
   });
 
   it('prout', async () => {
-    const el = await fixture('<super-lit-ts-button>Plop</super-lit-ts-button>');
+    const el = await fixture<ElementClass>(
+      `<${elementTag}>Plop</${elementTag}>`
+    );
     const btn = el.shadowRoot?.querySelector('button');
-
     expect(btn?.classList.contains('primary')).to.true;
   });
 
   it('prout', async () => {
-    const el = await fixture(
-      '<super-lit-ts-button theme="danger">Plop</super-lit-ts-button>'
+    const el = await fixture<ElementClass>(
+      `<${elementTag}>Plop</${elementTag}>`
+    );
+    setTimeout(() => el.click());
+
+    const ev = await oneEvent(el, 'click');
+    expect(ev).to.exist;
+  });
+
+  it('prout', async () => {
+    const el = await fixture<ElementClass>(
+      `<${elementTag} theme="danger">Plop</${elementTag}>`
     );
     const btn = el.shadowRoot?.querySelector('button');
 

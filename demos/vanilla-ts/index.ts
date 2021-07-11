1c73f8768697621b3231552e6243a1fdfa7f4955
Al-un <alun.sng@gmail.com>
2021-07-11 19:54:21 +0900

    demo-ts: WIP TypeScript+Webpack demo

---
+++ demos/vanilla-ts/index.ts
@@ -0,0 +1,1 @@
+import '@al-un/wcl-lit-ts';
---
+++ demos/vanilla-ts/webpack.config.ts
@@ -0,0 +1,23 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
+
+// TS config: https://webpack.js.org/configuration/configuration-languages/#typescript
+const config: Configuration = {
+  mode: 'development',
+
+  entry: { main: './index.ts' },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+  ],
+
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader' }],
+  },
+};
+
+export default config;

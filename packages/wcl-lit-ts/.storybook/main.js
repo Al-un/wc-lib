53d4a4c22cd830629c43cc7dd93d717a264027eb
Al-un <alun.sng@gmail.com>
2021-07-11 22:57:25 +0900

    lit-ts: add webpack5 configuration for storybook

--- packages/wcl-lit-ts/.storybook/main.js
+++ packages/wcl-lit-ts/.storybook/main.js
@@ -1,12 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#webpack-5-manager-build
+  // https://github.com/storybookjs/storybook/pull/15001
+  core: {
+    builder: 'webpack5',
+  },
+
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     // Added as implicit PostCSS is deprecated from Storybook 6.2 but it has no
     // effect as sass-loader seems to override the postcss configuration
+    // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#deprecated-implicit-postcss-loader
+    // https://github.com/storybookjs/addon-postcss
     '@storybook/addon-postcss',
   ],
 

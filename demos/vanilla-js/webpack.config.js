190be577fe4441131e4f98aaa30ed3a1a8fdfd39
Al-un <alun.sng@gmail.com>
2021-07-11 20:13:16 +0900

    demo-js: reorganise package

--- demos/vanilla-js/webpack.config.js
+++ demos/vanilla-js/webpack.config.js
@@ -8,15 +8,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   mode: isProduction ? 'production' : 'development',
 
-  entry: { main: './index.js' },
+  entry: { main: './src/index.js' },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'src/index.html',
       filename: 'index.html',
       chunks: ['main'],
     }),
   ],
-
-  watch: !isProduction,
 };
